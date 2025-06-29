8d6677533ac956eb34b05aafc5e7b19f995873f3
adhi <adzkiyadzikrullah@gmail.com>
2025-06-29 21:33:11 +0700

    add: recent projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@ import Hero from "./components/hero";
 import { FloatingNav } from "./components/ui/floating-navbar";
 import Grid from "./components/grid";
 import { navItems } from "@/data";
+import { Experience } from "./components/experience";
+import RecentProject from "./components/recent-project";
 
 export default function Home() {
   return (
-    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="bg-black-100 relative flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <Experience />
+        <RecentProject />
       </div>
     </main>
   );
